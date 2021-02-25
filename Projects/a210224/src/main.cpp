916f90335dbc095b1b691f5eac306f1950032199
Ricardo Ardissone <ricardo.ardissone@gmail.com>
2021-02-24 23:02:06 -0300

    lab1

--- Projects/a210224/src/main.cpp
+++ Projects/a210224/src/main.cpp
@@ -1,5 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
+// includes da biblioteca driverlib
+#include "inc/hw_memmap.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
 
-int main()
-{
-  return 0;
-}
+uint8_t LED_D4 = 0;
+
+void main(void){    
+  SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF); // Habilita GPIO F (LED D3 = PF4, LED D4 = PF0)
+  while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF)); // Aguarda final da habilitação
+    
+  GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_0 ); // LEDs D4 como saída
+  GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, 0); // LEDs D4 apagados
+  GPIOPadConfigSet(GPIO_PORTF_BASE, GPIO_PIN_0, GPIO_STRENGTH_12MA, GPIO_PIN_TYPE_STD);
+
+  while(1){
+    int i = 0;
+    // No optimization -> 5 instructions. 23s
+    // Low optimization -> 4 instructions.  Some take more than 1 cycle. 21s
+    // High optimizations -> Loop eliminated. No blinking.
+    // https://developer.arm.com/documentation/ddi0439/b/Programmers-Model/Instruction-set-summary/Cortex-M4-instructions
+    // Low optimization on 120MHz -> 7s
+    for(i = 0; i <= 24000000/6; i++);
+    
+    LED_D4 ^= GPIO_PIN_0; // Troca estado do LED D4
+    GPIOPinWrite(GPIO_PORTF_BASE, GPIO_PIN_0, LED_D4); // Acende ou apaga LED D4
+  }
+} // main
